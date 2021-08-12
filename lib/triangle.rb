e099ddb7374346205adf76aaff6f4901d0dc1967
Whiting <whitinghtennis@gmail.com>
2021-08-12 10:29:33 -0700

    commit tree b828f4a3515962d82884b50def3434a19ba9f908

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+attr_reader :side1, :side2, :side3, :perim
+
+
+    def initialize(side1, side2, side3)
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+      @perim = [side1, side2, side3]
+    end
+
+    def kind
+      # if @perim.inject(0, :+) != 180
+      #   raise TriangleError
+      if @perim.any? { |side| side <= 0 }
+        raise TriangleError
+      elsif @side1 + @side2 <= @side3
+        raise TriangleError
+      elsif @side2 + @side3 <= @side1
+        raise TriangleError
+      elsif @side1 + @side3 <= @side2
+        raise TriangleError
+      elsif @perim.detect{ |side| @perim.count(side) == 3 }
+        :equilateral
+      elsif @perim.detect{ |side| @perim.count(side) == 2 }
+        :isosceles
+      else :scalene
+      end
+    end
+   
+
+    class TriangleError < StandardError
+      def message
+        "wack triangle, yo"
+      end
+    end
+
 end
